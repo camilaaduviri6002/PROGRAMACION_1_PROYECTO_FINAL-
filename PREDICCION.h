8fa1a6871a48c6221b3784865e9240df6bc707f6
Alejandra Romero Aro <alejandra.romero.a@ucb.edu.bo>
2026-06-24 14:34:21 -0400

    Remove redundant newlines in PREDICCION.h

--- PREDICCION.h
+++ PREDICCION.h
@@ -1,18 +1,11 @@
 #ifndef PREDICCION_H_INCLUDED
 #define PREDICCION_H_INCLUDED
 
-
-
 float calcularIndiceOfensivo(int codigoEquipo);
-
 float calcularIndiceDefensivo(int codigoEquipo);
-
 float calcularProbabilidadLocal(int codigoLocal, int codigoVisitante);
-
 float calcularProbabilidadEmpate(int codigoLocal,int codigoVisitante);
-
 float calcularProbabilidadVisitante(int codigoLocal,int codigoVisitante);
-
 void mostrarPrediccion();
 
 
