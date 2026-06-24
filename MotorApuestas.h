e436aba940cb6f609a68d81f2c7140e57c2d5956
camilaaduviri6002 <camila.aduviri@ucb.edu.bo>
2026-06-24 13:01:34 -0400

    Update MotorApuestas.h

--- MotorApuestas.h
+++ MotorApuestas.h
@@ -3,260 +3,268 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
+const int MAX_APUESTAS = 100;
+const int MAX_EQUIPOS  = 20;
+
 struct Apuesta
 {
-    int id;
-    string usuario;
-    string equipo1;
-    string equipo2;
-    string resultado;
+    char username[30];
+    int codigoLocal;
+    int codigoVisitante;
+    int opcion;
     float monto;
+    float cuota;
+    bool resuelta;
+    float ganancia;
 };
 
-void RegistrarApuesta(Apuesta apuestas[], int &n)
+struct Partido
 {
-    cout << "\nID: "; 
-    cin >> apuestas[n].id;
+    int codigoLocal;
+    int codigoVisitante;
+    int golesLocal;
+    int golesVisitante;
+    bool jugado;
+};
 
+extern Apuesta apuestas[MAX_APUESTAS];
+extern int matrizGoles[MAX_EQUIPOS][MAX_EQUIPOS];
+extern int matrizVictorias[MAX_EQUIPOS][MAX_EQUIPOS];
+extern int nUsuarios;
+extern int nEquipos;
+extern int nApuestas;
+
+void RegistrarApuesta()
+{
+    cout << "\nUsuario: ";
     cin.ignore();
+    cin.getline(apuestas[nApuestas].username, 30);
 
-    cout << "Usuario: "; 
-    getline(cin, apuestas[n].usuario);
+    cout << "Codigo Equipo Local: ";
+    cin >> apuestas[nApuestas].codigoLocal;
 
-    cout << "Equipo Local: ";
-    getline(cin, apuestas[n].equipo1);
+    cout << "Codigo Equipo Visitante: ";
+    cin >> apuestas[nApuestas].codigoVisitante;
 
-    cout << "Equipo Visitante: "; 
-    getline(cin, apuestas[n].equipo2);
-
-    cout << "Resultado (Local/Empate/Visitante): "; 
-    getline(cin, apuestas[n].resultado);
+    cout << "Opcion (1=Local/2=Empate/3=Visitante): ";
+    cin >> apuestas[nApuestas].opcion;
 
     cout << "Monto: ";
-    cin >> apuestas[n].monto;
+    cin >> apuestas[nApuestas].monto;
 
-    n++;
+    cout << "Cuota: ";
+    cin >> apuestas[nApuestas].cuota;
+
+    apuestas[nApuestas].resuelta = false;
+    apuestas[nApuestas].ganancia = 0;
+
+    nApuestas++;
 }
 
-void MostrarApuestas(Apuesta apuestas[], int n, int pos)
+void MostrarApuestas(int pos)
 {
-    if(pos >= n)
+    if(pos >= nApuestas)
         return;
 
     cout << "\n------------------------";
-    cout << "\nID: " << apuestas[pos].id;
-    cout << "\nUsuario: " << apuestas[pos].usuario;
-    cout << "\nPartido: "
-         << apuestas[pos].equipo1
-         << " vs "
-         << apuestas[pos].equipo2;
-    cout << "\nResultado: "
-         << apuestas[pos].resultado;
+    cout << "\nUsuario: " << apuestas[pos].username;
+    cout << "\nPartido: Equipo "
+         << apuestas[pos].codigoLocal
+         << " vs Equipo "
+         << apuestas[pos].codigoVisitante;
+    cout << "\nOpcion: "
+         << apuestas[pos].opcion;
     cout << "\nMonto: "
          << apuestas[pos].monto;
+    cout << "\nCuota: "
+         << apuestas[pos].cuota;
+    cout << "\nResuelta: "
+         << (apuestas[pos].resuelta ? "Si" : "No");
+    cout << "\nGanancia: "
+         << apuestas[pos].ganancia;
 
-    MostrarApuestas(apuestas, n, pos + 1);
+    MostrarApuestas(pos + 1);
 }
 
-float TotalApostado(Apuesta apuestas[], int n, int pos)
+float TotalApostado(int pos)
 {
-    if(pos >= n)
+    if(pos >= nApuestas)
         return 0;
 
-    return apuestas[pos].monto +
-           TotalApostado(apuestas, n, pos + 1);
+    return apuestas[pos].monto + TotalApostado(pos + 1);
 }
 
-int BuscarApuesta(Apuesta apuestas[],
-                  int n,
-                  int codigo,
+int BuscarApuesta(int codigoLocal,
+                  int codigoVisitante,
+                  const char usuario[30],
                   int pos)
 {
-    if(pos >= n)
+    if(pos >= nApuestas)
         return -1;
 
-    if(apuestas[pos].id == codigo)
+    if(apuestas[pos].codigoLocal == codigoLocal &&
+       apuestas[pos].codigoVisitante == codigoVisitante &&
+       strcmp(apuestas[pos].username, usuario) == 0)
         return pos;
 
     return BuscarApuesta(
-            apuestas,
-            n,
-            codigo,
+            codigoLocal,
+            codigoVisitante,
+            usuario,
             pos + 1);
 }
 
-void GuardarApuestas(Apuesta apuestas[],
-                     int n,
-                     int pos,
-                     ofstream &archivo)
+void GuardarApuestas(int pos, ofstream &archivo)
 {
-    if(pos >= n)
+    if(pos >= nApuestas)
         return;
 
     archivo
-    << apuestas[pos].id << "|"
-    << apuestas[pos].usuario << "|"
-    << apuestas[pos].equipo1 << "|"
-    << apuestas[pos].equipo2 << "|"
-    << apuestas[pos].resultado << "|"
-    << apuestas[pos].monto
+    << apuestas[pos].username << "|"
+    << apuestas[pos].codigoLocal << "|"
+    << apuestas[pos].codigoVisitante << "|"
+    << apuestas[pos].opcion << "|"
+    << apuestas[pos].monto << "|"
+    << apuestas[pos].cuota << "|"
+    << apuestas[pos].resuelta << "|"
+    << apuestas[pos].ganancia
     << endl;
 
-    GuardarApuestas(
-        apuestas,
-        n,
-        pos + 1,
-        archivo);
+    GuardarApuestas(pos + 1, archivo);
 }
 
-void GuardarTodo(Apuesta apuestas[], int n)
+void GuardarTodo()
 {
     ofstream archivo("Apuestas.txt");
 
     if(!archivo)
         return;
 
-    GuardarApuestas(
-        apuestas,
-        n,
-        0,
-        archivo);
+    GuardarApuestas(0, archivo);
 
     archivo.close();
 }
 
-void CargarApuestas(Apuesta apuestas[], int &n)
+void CargarApuestas()
 {
     ifstream archivo("Apuestas.txt");
 
     if(!archivo)
         return;
 
-    n = 0;
+    nApuestas = 0;
 
-    while(archivo >> apuestas[n].id)
+    string linea;
+    while(getline(archivo, linea))
     {
-        archivo.ignore();
+        if(linea.empty())
+            continue;
 
-        getline(
-            archivo,
-            apuestas[n].usuario,
-            '|');
+        size_t p1 = linea.find('|');
+        size_t p2 = linea.find('|', p1 + 1);
+        size_t p3 = linea.find('|', p2 + 1);
+        size_t p4 = linea.find('|', p3 + 1);
+        size_t p5 = linea.find('|', p4 + 1);
+        size_t p6 = linea.find('|', p5 + 1);
+        size_t p7 = linea.find('|', p6 + 1);
 
-        getline(
-            archivo,
-            apuestas[n].equipo1,
-            '|');
+        string usuarioStr = linea.substr(0, p1);
+        strcpy(apuestas[nApuestas].username, usuarioStr.c_str());
 
-        getline(
-            archivo,
-            apuestas[n].equipo2,
-            '|');
+        apuestas[nApuestas].codigoLocal =
+            stoi(linea.substr(p1 + 1, p2 - p1 - 1));
 
-        getline(
-            archivo,
-            apuestas[n].resultado,
-            '|');
+        apuestas[nApuestas].codigoVisitante =
+            stoi(linea.substr(p2 + 1, p3 - p2 - 1));
 
-        archivo >> apuestas[n].monto;
+        apuestas[nApuestas].opcion =
+            stoi(linea.substr(p3 + 1, p4 - p3 - 1));
 
-        archivo.ignore();
+        apuestas[nApuestas].monto =
+            stof(linea.substr(p4 + 1, p5 - p4 - 1));
 
-        n++;
+        apuestas[nApuestas].cuota =
+            stof(linea.substr(p5 + 1, p6 - p5 - 1));
+
+        apuestas[nApuestas].resuelta =
+            stoi(linea.substr(p6 + 1, p7 - p6 - 1)) != 0;
+
+        apuestas[nApuestas].ganancia =
+            stof(linea.substr(p7 + 1));
+
+        nApuestas++;
     }
 
     archivo.close();
 }
 
-void ReporteUsuario(Apuesta apuestas[],
-                    int n,
-                    string usuario,
-                    int pos)
+void ReporteUsuario(const char usuario[30], int pos)
 {
-    if(pos >= n)
+    if(pos >= nApuestas)
         return;
 
-    if(apuestas[pos].usuario == usuario)
+    if(strcmp(apuestas[pos].username, usuario) == 0)
     {
         cout << "\n------------------------";
-        cout << "\nID: "
-             << apuestas[pos].id;
-
         cout << "\nUsuario: "
-             << apuestas[pos].usuario;
+             << apuestas[pos].username;
 
-        cout << "\nPartido: "
-             << apuestas[pos].equipo1
-             << " vs "
-             << apuestas[pos].equipo2;
+        cout << "\nPartido: Equipo "
+             << apuestas[pos].codigoLocal
+             << " vs Equipo "
+             << apuestas[pos].codigoVisitante;
 
-        cout << "\nResultado: "
-             << apuestas[pos].resultado;
+        cout << "\nOpcion: "
+             << apuestas[pos].opcion;
 
         cout << "\nMonto: "
              << apuestas[pos].monto;
+
+        cout << "\nCuota: "
+             << apuestas[pos].cuota;
+
+        cout << "\nResuelta: "
+             << (apuestas[pos].resuelta ? "Si" : "No");
+
+        cout << "\nGanancia: "
+             << apuestas[pos].ganancia;
     }
 
-    ReporteUsuario(
-        apuestas,
-        n,
-        usuario,
-        pos + 1);
+    ReporteUsuario(usuario, pos + 1);
 }
 
-float TotalUsuario(Apuesta apuestas[],
-                   int n,
-                   string usuario,
-                   int pos)
+float TotalUsuario(const char usuario[30], int pos)
 {
-    if(pos >= n)
+    if(pos >= nApuestas)
         return 0;
 
-    if(apuestas[pos].usuario == usuario)
+    if(strcmp(apuestas[pos].username, usuario) == 0)
     {
         return apuestas[pos].monto +
-               TotalUsuario(
-                   apuestas,
-                   n,
-                   usuario,
-                   pos + 1);
+               TotalUsuario(usuario, pos + 1);
     }
 
-    return TotalUsuario(
-               apuestas,
-               n,
-               usuario,
-               pos + 1);
+    return TotalUsuario(usuario, pos + 1);
 }
 
-void BuscarUsuario(Apuesta apuestas[],
-                   int n)
+void BuscarUsuario()
 {
-    string usuario;
+    char usuario[30];
 
     cin.ignore();
 
     cout << "\nIngrese nombre del usuario: ";
-    getline(cin, usuario);
+    cin.getline(usuario, 30);
 
     cout << "\n========== REPORTE DEL USUARIO ==========";
 
-    ReporteUsuario(
-        apuestas,
-        n,
-        usuario,
-        0);
+    ReporteUsuario(usuario, 0);
 
-    cout << "\n\nTotal Apostado: "
-         << TotalUsuario(
-                apuestas,
-                n,
-                usuario,
-                0);
+    cout << "\n\nTotal Apostado: " << TotalUsuario(usuario, 0);
 
     cout << "\n=========================================";
 }
